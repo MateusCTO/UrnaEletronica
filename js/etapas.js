b587d8e96481b882c1d4922418805eab24ba1613
MateusCTO <farias.mateus@gmail.com>
2022-10-23 00:01:03 +0100

    project finished

---
+++ js/etapas.js
@@ -0,0 +1,60 @@
+let etapas = [
+    {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "12345",
+        nome: "Fulana de Tal",
+        partido: "ABC",
+        fotos: [
+          {url:"12345.jpg", legenda: "VEREADOR", small: false}
+        ]
+      },
+      {
+        numero: "67890",
+        nome: "Beltrana de Souza",
+        partido: "DEF",
+        fotos: [
+          {url:"67890.jpg", legenda: "VEREADOR", small: false}
+        ]
+      } 
+    ]
+    },
+    {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+      numero: "99",
+      nome: "Ciclano",
+      partido: "ABC",
+      vice: "Tucano",
+      fotos: [
+        {url:"99.jpg", legenda: "PREFEITO"},
+        {url:"99-2.jpg", legenda: "VICE-PREFEITO", small: true}
+      ]
+    },
+    {
+      numero: "88",
+      nome: "Joao",
+      partido: "DEF",
+      vice: "Gomes",
+      fotos: [
+        {url:"88.jpg", legenda: "PREFEITO"},
+        {url:"88-2.jpg", legenda: "VICE-PREFEITO", small: true}
+      ]
+    },
+    {
+      numero: "77",
+      nome: "Joana",
+      partido: "GHI",
+      vice: "Maria",
+      fotos: [
+        {url:"77.jpg", legenda: "PREFEITO"},
+        {url:"77-2.jpg", legenda: "VICE-PREFEITO", small: true}
+      ]
+    },
+  ]
+  }
+  ];
